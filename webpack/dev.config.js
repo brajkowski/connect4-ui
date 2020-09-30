a2873cf80efc6c2e4ae598f16eb05eabf1ca97cd
Brandon <brandon.rajkowski@gmail.com>
2020-09-29 22:07:25 -0600

    added image file types to load with file-loader

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -14,6 +14,10 @@ module.exports = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: 'file-loader',
+      },
     ],
   },
   resolve: {
