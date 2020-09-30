a3e9f0ea00aab2cc2338c6ecddfff44f85ddeb32
Brandon <brandon.rajkowski@gmail.com>
2020-09-29 22:50:16 -0600

    now loading and playing the click sound on pointerdown event

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { AUTO, Game, Scene, Types } from 'phaser';
 import background from './assets/background.png';
+import click from './assets/click.wav';
 
 const config: Types.Core.GameConfig = {
   type: AUTO,
@@ -21,8 +22,13 @@ const game = new Game(config);
 
 function preload(this: Scene) {
   this.load.image('background', background);
+  this.load.audio('click', click);
 }
 
 function create(this: Scene) {
   this.add.image(0, 0, 'background').setOrigin(0, 0);
+  const music = this.sound.add('click');
+  this.input.on('pointerdown', () => {
+    music.play();
+  });
 }
