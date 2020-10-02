d27cd7734d1b8639e55eb286bafff26319eb3daf
Brandon <brandon.rajkowski@gmail.com>
2020-10-02 00:00:05 -0600

    removed unneeded private members

--- src/logic/bitboard-player-state.ts
+++ src/logic/bitboard-player-state.ts
@@ -3,8 +3,6 @@ import { PlayerState } from './player-state';
 import { PositionAlreadyOccupiedError } from './position-already-occupied-error';
 
 export class BitboardPlayerState implements PlayerState {
-  private readonly empty = 0x0;
-  private readonly one = 0x1;
   private state: number;
 
   constructor();
@@ -17,16 +15,16 @@ export class BitboardPlayerState implements PlayerState {
   }
 
   clearAllPositions(): void {
-    this.state = this.empty;
+    this.state = 0;
   }
 
   occupiesPosition(row: number, column: number): boolean {
-    const mask = this.one << this.getShift(row, column);
+    const mask = 1 << this.getShift(row, column);
     return this.occupiesPositionByMask(mask);
   }
 
   occupyPosition(row: number, column: number): void {
-    const mask = this.one << this.getShift(row, column);
+    const mask = 1 << this.getShift(row, column);
     if (this.occupiesPositionByMask(mask)) {
       const message = `"Row ${row} column ${column} is already occupied"`;
       throw new PositionAlreadyOccupiedError(message);
