550d4e26f332162391c5611b412f3b05cd5a8cce
Brandon <brandon.rajkowski@gmail.com>
2020-10-27 00:21:25 -0600

    added additional audio formats

--- src/assets.ts
+++ src/assets.ts
@@ -7,3 +7,18 @@ declare module '*.wav' {
   const content: any;
   export default content;
 }
+
+declare module '*.ogg' {
+  const content: any;
+  export default content;
+}
+
+declare module '*.m4a' {
+  const content: any;
+  export default content;
+}
+
+declare module '*.mp3' {
+  const content: any;
+  export default content;
+}
