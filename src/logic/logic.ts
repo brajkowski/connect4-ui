870d1ae07cb31a9c7b8add14452de54e09d80171
Brandon <brandon.rajkowski@gmail.com>
2020-11-25 12:00:13 -0700

    boardIsFull is now part of the logic interface

--- src/logic/logic.ts
+++ src/logic/logic.ts
@@ -4,6 +4,7 @@ export interface Logic {
   getPlayerState(player: Player): PlayerState;
   placeChip(player: Player, column: number): number;
   didWin(player: Player): boolean;
+  boardIsFull(): boolean;
   canPlaceChip(column: number): boolean;
   clear(): void;
 }
