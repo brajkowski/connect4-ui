62dd14965d31887c29f2b9ed21e7184e07075218
Brandon <brandon.rajkowski@gmail.com>
2020-12-06 20:11:27 -0700

    added StrategyRule

---
+++ src/ai/strategies/strategy-rule.ts
@@ -0,0 +1,36 @@
+import { Logic, Player } from '../../logic/logic';
+import { QueryOptimizer } from '../queries/optimization/query-optimizer';
+
+export interface StrategyRule {
+  (player: Player, logic: Logic, optimizer?: QueryOptimizer): number;
+}
+
+export class StrategyRuleBuilder {
+  private rule: StrategyRule;
+
+  constructor(rule: StrategyRule) {
+    this.rule = rule;
+  }
+
+  orElse(rule: StrategyRule): StrategyRuleBuilder {
+    const last = this.rule;
+    this.rule = (p, l) => {
+      const result = last(p, l);
+      if (result == null) {
+        return rule(p, l);
+      }
+      return result;
+    };
+    return this;
+  }
+
+  finally(fallback: number): StrategyRule {
+    return (p, l) => {
+      const result = this.rule(p, l);
+      if (result == null) {
+        return fallback;
+      }
+      return result;
+    };
+  }
+}
