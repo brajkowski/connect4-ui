be2707400df4cd5afa261cb317023160ba637521
Brandon <brandon.rajkowski@gmail.com>
2020-12-28 21:42:50 -0700

    added all scenes

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
 import { AUTO, Game, Types } from 'phaser';
 import { MenuScene } from './gui/scenes/menu-scene';
+import { PlayingScene } from './gui/scenes/playing-scene';
 import { globalScale } from './gui/util/scale';
 
-const scene = new MenuScene(null);
+const menu = new MenuScene({ key: 'menu' });
+const easy = new PlayingScene({ key: 'easy' });
+const medium = new PlayingScene({ key: 'medium' });
+const hard = new PlayingScene({ key: 'hard' });
+const local = new PlayingScene({ key: 'local' });
+const multiplayer = new PlayingScene({ key: 'multi' });
 
 const config: Types.Core.GameConfig = {
   type: AUTO,
   width: globalScale(600),
   height: globalScale(600),
-  scene,
+  scene: [menu, easy, medium, hard, local, multiplayer],
   scale: {
     mode: Phaser.Scale.FIT,
   },
