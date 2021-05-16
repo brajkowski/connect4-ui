bacde24f992e9d2b4128158e65abc08354ea5d8c
Brandon Rajkowski <github@brajkowski.bulc.club>
2021-05-16 17:56:14 -0600

    added additional client handlers, games can now be restarted by the multiplayer server

--- src/gui/scenes/multiplayer-scene.ts
+++ src/gui/scenes/multiplayer-scene.ts
@@ -1,7 +1,7 @@
 import { Player } from '@brajkowski/connect4-logic';
 import { Connect4Client } from '@brajkowski/connect4-multiplayer-client';
 import { GameObjects, Types } from 'phaser';
-import { multiplayerServer } from '../..';
+import { multiplayerServer } from '../../index';
 import { PlayerController } from '../controllers/player-controller';
 import { globalScale } from '../util/scale';
 import { PlayingScene } from './playing-scene';
@@ -31,24 +31,14 @@ export class MultiplayerPlayingScene extends PlayingScene {
   }
 
   create() {
-    this.client.open(multiplayerServer);
     if (this.joinSession) {
       this.activePlayer = Player.Two;
     }
-    setTimeout(() => {
-      this.client.onOpponentMove((column) => super.dropChip(column));
-      if (this.joinSession) {
-        this.client.joinSession(
-          window.prompt('Join session:'),
-          this.displayName
-        );
-      } else {
-        this.client.createSession(this.displayName);
-        this.client.onSessionCreated((session) =>
-          console.log('Session created:', session)
-        );
-      }
-    }, 500);
+    this.client.open(multiplayerServer);
+    this.client.onOpen(() => {
+      this.setupSession();
+      this.setupClientHandlers();
+    });
     super.create();
     this.displayNameText = this.make.text({
       x: globalScale(160),
@@ -73,5 +63,29 @@ export class MultiplayerPlayingScene extends PlayingScene {
     this.client.onOpponentJoin((opponentDisplayName) =>
       this.opponentDisplayNameText.setText(opponentDisplayName)
     );
+  }
+
+  private setupSession() {
+    if (this.joinSession) {
+      this.client.joinSession(window.prompt('Join session:'), this.displayName);
+      this.client.onJoinedSession((opponentDisplayName) =>
+        this.opponentDisplayNameText.setText(opponentDisplayName)
+      );
+    } else {
+      this.client.createSession(this.displayName);
+      this.client.onSessionCreated((session) =>
+        console.log('Session created:', session)
+      );
+    }
+  }
+
+  private setupClientHandlers() {
+    this.client.onOpponentMove((column) => super.dropChip(column));
+    this.client.onGameRestart((thisClientStartsFirst) => {
+      super.restart(() => (thisClientStartsFirst ? Player.One : Player.Two));
+    });
+    this.client.onClose(() => alert('Socket closed'));
+    this.client.onOpponentQuit(() => alert('Opponent has left'));
+    this.client.onSessionEnded(() => alert('The session has ended'));
   }
 }
